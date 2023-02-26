eef280e1b956a1ca0e90a782fe408eeeddd4002c
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2023-02-27 00:46:58 +0100

    updated datasets and added dataframe visualization

--- src/pretprocessor.py
+++ src/pretprocessor.py
@@ -1,16 +1,31 @@
 import argparse
 import pandas
 import logger
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+relevant_attributes = ["time", "BlkCnt", "CapMrktCurUSD", "DiffMean", "FeeMeanUSD", "FlowInExUSD", "HashRate", "NDF",
+                       "ROI30d", "PriceUSD"]
+
 
 def get_args():
-    parser = argparse.ArgumentParser(prog = "Preprocessor for datasets for crypto")
+    parser = argparse.ArgumentParser(prog="Preprocessor for datasets for crypto")
     parser.add_argument("path", type=str, help="Path to the dataset csv which should be loaded")
 
     return parser.parse_args()
 
+
 def read_csv(path):
-    df = pandas.read_csv(rf'{path}')
+    df = pandas.read_csv(rf'{path}',
+                         usecols=relevant_attributes
+                         )
     return df
+
+
+def plot_df_columns(df):
+    sns.pairplot(df)
+    plt.show()
+
 
 def main():
     log = logger.get_logger('pretprocessor')
@@ -18,11 +33,14 @@ def main():
     log.info(f"Starting preprocessor for {args.path}")
     df = read_csv(args.path)
 
+    ### Uncomment for graphical data representation
+    # plot_df_columns(df)
+
     ### Replace NaN with 0
     df = df.fillna(0)
 
     df.to_csv(args.path)
-    log.info(f"Preprocessing finished and dumped to {args.path}")
+    log.info(f"Preprocessing finished and dumped to {args.path} ")
 
 
 if __name__ == "__main__":
