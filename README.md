8a5f08872ff570117534725dcf54d376aedc24fe
NikolaMilosa <nikolamilosa20@gmail.com>
2023-02-26 14:54:43 +0100

    added logger and started preprocessing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# TODO
+# Dataset field meaning
+* Time - date when the reading was made
+* BlkCnt - number of blocks created for the day the reading corresponds to
+* CapMrktCurUSD - total current market value in dollars
+* DiffMean - mean difficulty of finding a hash
+* FeeMeanUSD - mean value of fee for a transaction 
+* FlowInExUSD - total crypto exchange between users in dollars
+* HashRate - mean rate of solving hashes 
+* NDF - network distribution factor
+* ROI30d - estimated return of invest in 30 days
---
+++ src/pretprocessor.py
@@ -0,0 +1,29 @@
+import argparse
+import pandas
+import logger
+
+def get_args():
+    parser = argparse.ArgumentParser(prog = "Preprocessor for datasets for crypto")
+    parser.add_argument("path", type=str, help="Path to the dataset csv which should be loaded")
+
+    return parser.parse_args()
+
+def read_csv(path):
+    df = pandas.read_csv(rf'{path}')
+    return df
+
+def main():
+    log = logger.get_logger('pretprocessor')
+    args = get_args()
+    log.info(f"Starting preprocessor for {args.path}")
+    df = read_csv(args.path)
+
+    ### Replace NaN with 0
+    df.fillna(0)
+
+    df.to_csv(args.path)
+    log.info(f"Preprocessing finished and dumped to {args.path}")
+
+
+if __name__ == "__main__":
+    main()
