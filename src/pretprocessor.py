b124d6df2e43a5d4cb93af3e9de30f112c2bba11
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2023-02-28 00:18:00 +0100

    added metadata(table images)

--- src/pretprocessor.py
+++ src/pretprocessor.py
@@ -3,6 +3,8 @@ import pandas
 import logger
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sys
+import numpy
 
 relevant_attributes = ["time", "BlkCnt", "CapMrktCurUSD", "DiffMean", "FeeMeanUSD", "FlowInExUSD", "HashRate", "NDF",
                        "ROI30d", "PriceUSD"]
@@ -30,21 +32,45 @@ def plot_df_columns(df):
     plt.show()
 
 
+def print_description(df):
+    numpy.set_printoptions(threshold=sys.maxsize)
+    ### Keeps output decimal
+    # print_val = df.describe().apply(lambda s: s.apply('{0:.2f}'.format))
+    print_val = df.describe().round(2)
+    print(print_val.to_string())
+
+
+def plot_histograms(df, bin_num):
+    for attribute in relevant_attributes:
+        if attribute != "time":
+            df[attribute].plot(kind='hist', bins=bin_num, title=attribute)
+            plt.show()
+
+
+def plot_histogram(df, bin_num):
+    df.hist(bins=bin_num)
+    plt.show()
+
+
 def main():
     log = logger.get_logger('pretprocessor')
     args = get_args()
     log.info(f"Starting preprocessor for {args.path}")
     df = read_csv(args.path)
 
-    ### Graphical data representation
-    if args.visualize:
-        plot_df_columns(df)
-
     ### Replace NaN with 0
     df = df.fillna(0)
 
-    df.to_csv(args.path)
-    log.info(f"Preprocessing finished and dumped to {args.path} ")
+    ### Graphical data representation
+    if args.visualize:
+        plot_df_columns(df)
+        plot_histogram(df, 40)
+        plot_histograms(df, 100)
+        print_description(df)
+
+    output_path = args.path.replace('input', 'output')
+    df.to_csv(output_path)
+    log.info(f"Preprocessing finished and dumped to {output_path} ")
 
 
 if __name__ == "__main__":
