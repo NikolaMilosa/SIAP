a039e8c02b2fa0af16ff2ed973ebb02f8faf1619
vrba3 <74838282+vrba3@users.noreply.github.com>
2023-02-28 22:17:00 +0100

    Added neural network

--- src/pretprocessor.py
+++ src/pretprocessor.py
@@ -5,6 +5,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
 import numpy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Dropout
 
 relevant_attributes = ["time", "BlkCnt", "CapMrktCurUSD", "DiffMean", "FeeMeanUSD", "FlowInExUSD", "HashRate", "NDF",
                        "ROI30d", "PriceUSD"]
@@ -51,6 +53,24 @@ def plot_histogram(df, bin_num):
     df.hist(bins=bin_num)
     plt.show()
 
+def make_neural_network(df):
+    df = df.describe().round(2)
+    df = df.to_numpy()
+
+    X = df[:,0:8]
+    Y = df[:, 8]
+    model = Sequential()
+    model.add(Dense(12, input_shape=(8,), activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # compile the keras model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit the keras model on the dataset
+    model.fit(X, Y, epochs=300, batch_size=64)
+    # evaluate the keras model
+    _, accuracy = model.evaluate(X, Y)
+    print('Accuracy: %.2f' % (accuracy * 100))
 
 def main():
     log = logger.get_logger('pretprocessor')
@@ -60,7 +80,7 @@ def main():
 
     ### Replace NaN with 0
     df = df.fillna(0)
-
+    make_neural_network(df)
     ### Graphical data representation
     if args.visualize:
         plot_df_columns(df)
