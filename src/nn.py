d46419a9c573509994ec3c790000cb41ddf9102b
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2023-05-07 01:18:32 +0200

    11k loss

--- src/nn.py
+++ src/nn.py
@@ -1,11 +1,10 @@
 import torch
 import torch.nn as nn
-import json
 from sklearn.model_selection import train_test_split
 
 # 1D convolutional neural network
 from torch import optim
-from torch.utils.data import DataLoader
+
 
 from src.constants import output_attribute
 
@@ -15,12 +14,10 @@ torch.manual_seed(42)
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(9, 32)
-        self.fc2 = nn.Linear(32, 64)
-        self.fc3 = nn.Linear(64, 128)
-        self.fc4 = nn.Linear(128, 64)
-        self.fc5 = nn.Linear(64, 32)
-        self.fc6 = nn.Linear(32, 1)
+        self.fc1 = nn.Linear(9, 64)
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 1)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.2)
 
@@ -31,30 +28,18 @@ class Net(nn.Module):
         x = self.dropout(x)
         x = self.relu(self.fc3(x))
         x = self.dropout(x)
-        x = self.relu(self.fc4(x))
-        x = self.dropout(x)
-        x = self.relu(self.fc5(x))
-        x = self.dropout(x)
-        x = self.fc6(x)
+        x = self.fc4(x)
         return x
 
 
 def create_neural_network(data, path, epoch_num):
     # Extract the input and output data
     x = data.drop([output_attribute], axis=1).values
-    # y = data[output_attribute].values.astype('float32')
     y = data[output_attribute].values.reshape(-1, 1)
 
     # Split the dataset into training and testing sets
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-    # Convert the data to PyTorch tensors and create datasets and dataloaders
-    # train_dataset = torch.utils.data.TensorDataset(torch.tensor(x_train, dtype=torch.float32),
-    #                                                torch.tensor(y_train, dtype=torch.float32))
-    # test_dataset = torch.utils.data.TensorDataset(torch.tensor(x_test, dtype=torch.float32),
-    #                                               torch.tensor(y_test, dtype=torch.float32))
-    # train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    # test_loader = DataLoader(test_dataset, batch_size=16)
 
     # Instantiate neural network model
     model = Net()
@@ -100,11 +85,4 @@ def create_neural_network(data, path, epoch_num):
             if i % 15 == 0:
                 print(f'{outputs[i].item()}\t{y_test[i]}')
 
-    # Serialize model and loss
-    # output_path = path.replace('input', 'output').replace('.csv', '.json')
-    # serialization_data = {
-    #     'loss_history': 0
-    # }
-    # with open(output_path, 'w') as f:
-    #     json.dump(serialization_data, f)
-    # torch.save(net.state_dict(), output_path.replace('output', 'output/model'))
+
